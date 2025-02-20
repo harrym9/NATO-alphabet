6d5f86531b0c5289d46a8d988bc8510c07b10f10
Gayrat Rahmatulayev <gayratrahmatulayev8@gmail.com>
2025-02-20 19:38:46 +0300

    non-alphabet characters exception handler added

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-
-user_input = input("Enter the name: ").upper()
-result = [nato_dict[letter] for letter in user_input]
+while True:
+    try:
+        user_input = input("Enter the name: ").upper()
+        result = [nato_dict[letter] for letter in user_input]
+        break
+    except KeyError:
+        print("Sorry, only letters in the English alphabet please.")
 
 print(result)
